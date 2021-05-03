f0594ec88c3b593563d81f64d82bbfa8be84ce57
Fajar Bagus Widiarno <widiarnofajarbagus@gmail.com>
2021-05-03 17:24:13 +0700

    new

--- lib/ui/screen/home/home.dart
+++ lib/ui/screen/home/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:papi_kost/ui/router/routergenerator.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,15 +9,20 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   ScrollController _scrollController = ScrollController();
 
+  bool haveNotif = false;
+  String activeList = 'Location';
+
+  @override
+  initState() {
+    super.initState();
+    _scrollController.addListener(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-    ));
     return Scaffold(
       body: Container(
-        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 25),
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 30),
         child: SingleChildScrollView(
           physics: BouncingScrollPhysics(),
           controller: _scrollController,
@@ -25,6 +30,10 @@ class _HomePageState extends State<HomePage> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               appBarHome(),
+              SizedBox(
+                height: 10,
+              ),
+              listCatergory(),
             ],
           ),
         ),
@@ -34,11 +43,121 @@ class _HomePageState extends State<HomePage> {
 
   Widget appBarHome() {
     return Container(
-      color: Colors.blue,
       height: MediaQuery.of(context).size.height * 0.1,
       child: Row(
-        children: [],
+        children: [
+          Expanded(
+            flex: 3,
+            child: searchWidget(),
+          ),
+          SizedBox(width: 10),
+          listIcon(),
+        ],
       ),
     );
+  }
+
+  Widget listIcon() {
+    return Container(
+      child: Row(
+        children: [
+          iconHome(Icons.favorite_border_rounded, function: null),
+          SizedBox(
+            width: 10,
+          ),
+          iconHome(
+            haveNotif
+                ? Icons.notifications_active_rounded
+                : Icons.notifications_none,
+            function: null,
+          ),
+          SizedBox(width: 10),
+          iconHome(Icons.account_circle_rounded, function: null),
+        ],
+      ),
+    );
+  }
+
+  Widget iconHome(IconData icon, {Function(bool) function}) {
+    return GestureDetector(
+      onTap: () {
+        function != null ? function(haveNotif) : print('belom ada function');
+      },
+      child: Icon(
+        icon,
+        color: Theme.of(context).buttonColor,
+      ),
+    );
+  }
+
+  Widget searchWidget() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.07,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(
+          color: Colors.grey.withOpacity(0.4),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(Icons.search),
+            SizedBox(width: 5),
+            Expanded(
+              child: TextField(
+                readOnly: true,
+                onTap: null,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Cari Kost ?',
+                  border: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<String> _list = ['Location', 'Food', 'Tradition', 'Hotels'];
+
+  Widget listCatergory() {
+    return SingleChildScrollView(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: _list
+            .map(
+              (e) => GestureDetector(
+                onTap: () => changePriorityList(e),
+                child: Container(
+                  child: Text(
+                    e,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: activeList == e
+                          ? Theme.of(context).buttonColor
+                          : Theme.of(context).accentColor,
+                    ),
+                  ),
+                ),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  void changePriorityList(String e) {
+    setState(() {
+      activeList = e;
+    });
   }
 }
